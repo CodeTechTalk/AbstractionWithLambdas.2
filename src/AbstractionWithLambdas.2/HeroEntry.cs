2f72ae36c3fc2bc828140c347441b98a69387406
Will Bellman <wbellman@gmail.com>
2018-09-16 13:27:07 -0500

    Code at the end of the video.

---
+++ src/AbstractionWithLambdas.2/HeroEntry.cs
@@ -0,0 +1,11 @@
+namespace AbstractionWithLambdas {
+  public class HeroEntry {
+    public int    Id       { get; set; }
+    public string Value    { get; set; }
+    public int    Computed { get; set; }
+    public string Name     { get; set; }
+    public string Universe { get; set; }
+    public int    Appeared { get; set; }
+    public int    Year     { get; set; }
+  }
+}
--- src/AbstractionWithLambdas.2/Program.cs
+++ src/AbstractionWithLambdas.2/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using Newtonsoft.Json;
 using NLog;
 
@@ -17,14 +20,39 @@ namespace AbstractionWithLambdas {
     
     public static void Main(string[] args) {
       Log.Info("Starting application...");
-      ListHeroes();
+      Time(ListHeroesDictionary);
+      Time(ListHeroesDataTable);
+      Time(ListHeroEntries);
+      Time(ListDataGeneric);
       Log.Info("Application complete.");
     }
     
     // See Schema.sql and Data.sql for info on creating
     // Tables for this demo.
+
+    public static void Time(Action action) {
+      var sw = new Stopwatch();
+      sw.Start();
+      action();
+      sw.Stop();
+      Log.Trace($"Process completed in {sw.ElapsedMilliseconds}ms");
+    }
     
-    public static void ListHeroes() {
+    public static void ListHeroesDataTable() {
+      Log.Info("Starting Data Table Process...");
+      Log.Info("Selecting and retrieving data...");
+      var data = GetDataTable(
+        "SELECT Id, Value, Computed, Name, Universe, Appeared, Year FROM dbo.LargeListWithCompute");
+
+      Log.Info("Serializing json...");
+      var json = JsonConvert.SerializeObject(data);
+
+      Log.Info("Writing file...");
+      File.WriteAllText("heroes.dt.out.txt", json);
+    }
+    
+    public static void ListHeroesDictionary() {
+      Log.Info("Starting Dictionary Process...");
       Log.Info("Selecting and retrieving data...");
       var data = GetData("SELECT Id, Value, Computed, Name, Universe, Appeared, Year FROM dbo.LargeListWithCompute")
                   .ToList();
@@ -33,7 +61,118 @@ namespace AbstractionWithLambdas {
       var json = JsonConvert.SerializeObject(data);
 
       Log.Info("Writing file...");
-      File.WriteAllText("heroes.out.txt", json);
+      File.WriteAllText("heroes.dict.out.txt", json);
+    }
+    
+    public static void ListHeroEntries() {
+      Log.Info("Starting Dictionary Process...");
+      Log.Info("Selecting and retrieving data...");
+      var data = GetHeroEntries("SELECT Id, Value, Computed, Name, Universe, Appeared, Year FROM dbo.LargeListWithCompute")
+                  .ToList();
+
+      Log.Info("Serializing json...");
+      var json = JsonConvert.SerializeObject(data);
+
+      Log.Info("Writing file...");
+      File.WriteAllText("heroes.obj.out.txt", json);
+    }
+    
+    public static void ListDataGeneric() {
+      Log.Info("Starting Dictionary Process...");
+      Log.Info("Selecting and retrieving data...");
+      var data = GetData<HeroEntry>("SELECT Id, Value, Computed, Name, Universe, Appeared, Year FROM dbo.LargeListWithCompute")
+                  .ToList();
+
+      Log.Info("Serializing json...");
+      var json = JsonConvert.SerializeObject(data);
+
+      Log.Info("Writing file...");
+      File.WriteAllText("heroes.generic.out.txt", json);
+    }
+    
+    private static DataTable GetDataTable(string query) {
+      var dt = new DataTable();
+
+      ExecuteQuery(query, reader => dt.Load(reader));
+
+      return dt;
+    }
+
+    private static string GetStr(IDataReader reader, string field) {
+      return reader[field] is DBNull
+        ? null
+        : reader[field].ToString();
+    }
+
+    private static int GetInt(IDataReader reader, string field) {
+      var obj = reader[field];
+      if (obj is DBNull) {
+        return default(int);
+      }
+
+      var str = obj.ToString();
+      if (string.IsNullOrWhiteSpace(str)) {
+        return default(int);
+      }
+
+      return Convert.ToInt32(str);
+    }
+    
+    private static IEnumerable<HeroEntry> GetHeroEntries(string query) {
+      var data = new List<HeroEntry>();
+
+      ExecuteQuery(query, reader => {
+        
+        while (reader.Read()) {
+          var item = new HeroEntry {
+            Name = GetStr(reader, "Name"),
+            Appeared = GetInt(reader, "Appeared"),
+            Computed = GetInt(reader, "Computed"),
+            Id = GetInt(reader, "Id"),
+            Universe = GetStr(reader, "Universe"),
+            Value = GetStr(reader, "Value"),
+            Year = GetInt(reader, "Year")
+          };
+        }
+      });
+
+      return data;
+    }
+    
+    private static IEnumerable<T> GetData<T>(string query) where T : new() {
+      var data = new List<T>();
+      var type = typeof(T);
+
+      ExecuteQuery(query, reader => {
+        
+        while (reader.Read()) {
+          var obj = Activator.CreateInstance<T>();
+
+          for (var i = 0; i < reader.FieldCount; i++) {
+            var field = reader.GetName(i);
+
+            var prop = type.GetProperty(field, BindingFlags.Public);
+
+            if (prop == null) {
+              continue;
+            }
+
+            var value = reader[i];
+            if (!prop.PropertyType.IsInstanceOfType(value)) {
+              continue;
+            }
+            
+            prop.SetValue(obj,value);
+
+          }
+
+          data.Add(obj);
+
+        }
+        
+      });
+
+      return data;
     }
 
     /****************************************
